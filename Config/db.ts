73d28c7fe53181d106c3f7d441963013d414f3c3
Morgan <Karypekito@gmail.com>
2026-06-24 07:27:06 -0600

    s

--- Config/db.ts
+++ Config/db.ts
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+/*
 const conexion = mysql.createPool({
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
@@ -21,5 +21,34 @@ export async function testDbConnection() {
         console.error(" Error al conectar a la base de datos:");
         console.error(error);
     }
+        
+
 }
+export default conexion; */
+
+const conexion = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 4, 
+  ssl: {
+    rejectUnauthorized: false 
+  },
+  timezone: '-06:00',
+});
+
+// Interceptar cada nueva conexión
+// Interceptar cada nueva conexión
+conexion.on('connection', async (connection: any) => {
+  try {
+    // Aquí el .promise() es la clave
+    await connection.promise().query("SET time_zone = '-06:00';");
+  } catch (err) {
+    console.error("Error configurando la zona horaria en la conexión:", err);
+  }
+});
+
 export default conexion;
